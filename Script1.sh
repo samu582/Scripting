dab855be4268394c9dee59ab4d334fc34f717f80
samu582 <samupisolkar582@gmail.com>
2025-03-06 18:58:45 +0530

    Updated File

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "We are trying this practical from visiual studio"
 echo "We are trying this practical from  GitHub"
-
+echo  "We are trying this for practice"
